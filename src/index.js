864f544d1a2258dff151056d3e3ab6744acdf2a7
Tim Wood <tim.wood@digicatapult.org.uk>
2026-07-22 12:23:10 +0100

    Add js algs to exercise cbom

--- src/index.js
+++ src/index.js
@@ -18,3 +18,29 @@ if (timeout != 0) {
     server.close(() => {});
   }, Number(timeout));
 }
+
+const {
+  generateKeyPairSync,
+  createHash,
+  createHmac,
+} = require('crypto');
+
+const { ml_dsa65 } = require('@noble/post-quantum/ml-dsa');
+
+// RSA
+generateKeyPairSync('rsa', { modulusLength: 2048 });
+
+// Ed25519
+generateKeyPairSync('ed25519');
+
+// ML-DSA
+ml_dsa65.keygen();
+
+// SHA-256
+createHash('sha256').update('test').digest('hex');
+
+// BLAKE2
+createHash('blake2b512').update('test').digest('hex');
+
+// HMAC-SHA256
+createHmac('sha256', 'secret').update('test').digest('hex');
